2acd5a369621abd74fe1291492c72c3642ee2ff0
mp40 <philps.matthew@gmail.com>
2021-08-25 17:07:28 +0200

    Update main to serve html

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
 
+	"github.com/joho/godotenv"
+	"github.com/mp40/dkk-to-aud/calculate"
 	"github.com/mp40/dkk-to-aud/read"
 )
 
-func main() {
+func getEnvVariable(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	values := read.Read()
 
-	fmt.Println(values)
+	data := calculate.GetResults(values)
+
+	html, err := template.ParseFiles("view.html")
+	check(err)
+
+	err = html.Execute(writer, data)
+	check(err)
+}
+
+func main() {
+	http.HandleFunc("/", viewHandler)
+	err := http.ListenAndServe(getEnvVariable("BASE_URL"), nil)
+	log.Fatal(err)
 }
