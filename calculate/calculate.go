8a230d0bb71eaf1305268f16fa9ec1e0912462e2
mp40 <philps.matthew@gmail.com>
2021-08-25 08:37:26 +0200

    Pass failing test for highest exchange rate

--- calculate/calculate.go
+++ calculate/calculate.go
@@ -1,8 +1,35 @@
 package calculate
 
+import (
+	"strconv"
+)
+
 func GetResults(values [][]interface{}) map[string]float64 {
+
+	high := 0.0
+
+	for _, v := range values {
+
+		var f float64
+		var err error
+
+		if str, ok := v[1].(string); ok {
+			f, err = strconv.ParseFloat(str, 64)
+
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			f = 0.0
+		}
+
+		if f > high {
+			high = f
+		}
+	}
+
 	results := map[string]float64{
-		"high":    0.223,
+		"high":    high,
 		"low":     0.199,
 		"average": 0.2112,
 	}
