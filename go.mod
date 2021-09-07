9850bdee4c270703f025094f222651fa7ab99ba9
mp40 <philps.matthew@gmail.com>
2021-09-07 15:37:19 +0200

    Add cache logic to main

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mp40/dkk-to-aud
 go 1.15
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/joho/godotenv v1.3.0
 	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f
 	google.golang.org/api v0.54.0
--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"text/template"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/mp40/dkk-to-aud/cache"
 	"github.com/mp40/dkk-to-aud/calculate"
 	"github.com/mp40/dkk-to-aud/read"
 )
@@ -25,10 +28,35 @@ func check(err error) {
 	}
 }
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	values := read.Read()
+func getCachedData() (*calculate.Data, error) {
+	cache := &cache.Cache
+	var data *calculate.Data
 
-	data := calculate.GetResults(values)
+	timestamp := time.Now()
+	key := timestamp.Format("2006-01-02")
+
+	cachedData, err := cache.Get(key)
+	if err != nil {
+		return data, err
+	}
+
+	var parseErr error
+	parseErr = json.Unmarshal([]byte(cachedData), &data)
+	if parseErr != nil {
+		return data, err
+	}
+
+	return data, nil
+}
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	data, cacheErr := getCachedData()
+
+	if cacheErr != nil {
+		values := read.Read()
+
+		data = calculate.GetResults(values)
+	}
 
 	html, err := template.ParseFiles("view.html")
 	check(err)
@@ -42,6 +70,8 @@ func main() {
 	if env != "production" {
 		loadEnvFile()
 	}
+
+	cache.CreateRedisCache()
 
 	port := os.Getenv("APP_PORT")
 
